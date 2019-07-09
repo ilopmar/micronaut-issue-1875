b0416b543980bbb69ef23c5f0f6178db180d1fdb
Iván López <lopez.ivan@gmail.com>
2019-07-09 11:31:18 +0200

    Initial commit

---
+++ src/main/java/validator/header/controller/Application.java
@@ -0,0 +1,10 @@
+package validator.header.controller;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
